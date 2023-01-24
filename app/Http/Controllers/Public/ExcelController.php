e7d06a632bd3bbeb7e7974c297dca1e7658c8b8f
Mustafa Yılmaz <mustafayilmaz_28@hotmail.com>
2023-01-24 20:11:41 +0300

    açıklamalar eklendi.

--- app/Http/Controllers/Public/ExcelController.php
+++ app/Http/Controllers/Public/ExcelController.php
@@ -8,11 +8,21 @@ use Illuminate\Http\Request;
 use App\Imports\UsersImport;
 use Maatwebsite\Excel\Facades\Excel;
 
+use App\Models\User;
+
 class ExcelController extends Controller
 {
+    /**
+     * input file dan gelen excel dosyasını db ye içe aktaran metod
+     * @Request $request
+     * */
     function importStore(Request $request){
+        /**
+         * benzersiz bir token oluşturup Excel::import sınıfına bu tokeni ve file değişkenini gönderiyoruz.*/
         $import_token=uniqid();
         $import = Excel::import(new UsersImport($import_token), $request->file);
+        $inserted_users = User::where('import_token',$import_token)->get();
+        dd($inserted_users);
         return redirect()->back();
     }
 }
--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -11,15 +11,21 @@ class UsersImport implements ToModel, WithHeadingRow
 {
     public $import_token;
 
+    /**
+     * __construct metodunda controllerdan gelen parametreti alıp sınıf özelliğine atıyoruz.
+     * @string  $import_token
+     * */
     public function __construct(string $import_token)
     {
         $this->import_token = $import_token;
     }
+
     /**
-     * @param array $row
-     *
-     * @return \Illuminate\Database\Eloquent\Model|null
-     */
+     * 
+     * ToModel den implements edilen UsersImport sınıfının dahili model metodu
+     * bu metod kendisine gelen excel verisini satır satır user tablosuna ekler
+     * return null
+     * */
     public function model(array $row)
     {
         $user =  new User([
